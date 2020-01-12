6f375c2efa2541a52ce40c4717840dc5e7adf83a
Steven Timberman <stim@mit.edu>
2020-01-12 16:37:44 -0500

    New Robot (playerbbbbb): new robot added, with helper classes

---
+++ src/playerbbbbb/MinerRobot.java
@@ -0,0 +1,15 @@
+package playerbbbbb;
+import battlecode.common.*;
+
+/**
+* For static methods that miners will use.
+* Each should (most likely) take an rc as input
+**/
+
+public class MinerRobot {
+
+  public static void sayHi() {
+    System.out.println("I'm a miner robot!");
+  }
+
+}
